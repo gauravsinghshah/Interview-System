405a2dfb6b547d43384293f353b9aa312d6f1c87
Sourabh Singh <specious0027@gmail.com>
2026-03-09 23:16:53 +0530

    first commit

---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const login = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded-2xl border-4 border-solid border-gray-500 p-12">
+        <div className="text-4xl font-bold text-[#20beff] uppercase">
+          get smart
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
